79c579abafcb3c978137f345b0debf81994367f6
HG King <hgmaxwellking@gmail.com>
2020-10-19 07:57:00 -0400

    it runs and looks like it works properly

---
+++ src/heuristic_counter.rs
@@ -0,0 +1,281 @@
+use regex::Regex;
+
+macro_rules! lazy_regex {
+    // `()` indicates that the macro takes no argument.
+    ($pattern:literal, $name:ident) => {
+        // The macro will expand into the contents of this block.
+        lazy_static! {
+            static ref $name: Regex = Regex::new($pattern).unwrap();
+        }
+    };
+}
+
+fn captures_len(input: &str, re: &Regex) -> i32 {
+    let mut text = &input[..];
+    let mut count = 0;
+
+    while let Some(i) = re.find(text) {
+        count += 1;
+        text = &text[i.end()..];
+    }
+
+    return count;
+}
+
+fn o_4_in_row(input: &str) -> i32 {
+    lazy_regex!(r"OOOO", RE);
+    captures_len(input, &*RE)
+}
+
+// this will count EXXXExxxE as one since the middle E gets consumed
+// method below will count the second sequence
+fn o_2_empty_3_in_row_single(input: &str) -> i32 {
+    lazy_regex!(r"EOOOE", RE);
+    captures_len(input, &*RE)
+}
+
+// this is funky because of a special case ExxxExxxE
+// so we will count the second ExxxE here
+fn o_2_empty_3_in_row_double(input: &str) -> i32 {
+    lazy_regex!(r"EOOOEOOOE", RE);
+    captures_len(input, &*RE)
+}
+
+fn o_2_empty_3_in_row(input: &str) -> i32 {
+    o_2_empty_3_in_row_single(input) + o_2_empty_3_in_row_double(input)
+}
+
+fn o_1_empty_3_in_row_left(input: &str) -> i32 {
+    lazy_regex!(r"EOOO[^EO]", RE);
+    captures_len(input, &*RE)
+}
+
+fn o_1_empty_3_in_row_right(input: &str) -> i32 {
+    lazy_regex!(r"[^EO]OOOE", RE);
+    captures_len(input, &*RE)
+}
+
+fn o_1_empty_3_in_row(input: &str) -> i32 {
+    o_1_empty_3_in_row_right(input) + o_1_empty_3_in_row_left(input)
+}
+
+fn o_2_empty_2_in_row_single(input: &str) -> i32 {
+    lazy_regex!(r"EOOE", RE);
+    captures_len(input, &*RE)
+}
+
+// this is funky because of a special case ExxxExxxE
+// so we will count the second ExxxE here
+fn o_2_empty_2_in_row_double(input: &str) -> i32 {
+    lazy_regex!(r"EOOEOOE", RE);
+    captures_len(input, &*RE)
+}
+
+fn o_2_empty_2_in_row(input: &str) -> i32 {
+    o_2_empty_2_in_row_single(input) + o_2_empty_2_in_row_double(input)
+}
+
+fn o_1_empty_2_in_row_left(input: &str) -> i32 {
+    lazy_regex!(r"EOO[^EO]", RE);
+    captures_len(input, &*RE)
+}
+
+fn o_1_empty_2_in_row_right(input: &str) -> i32 {
+    lazy_regex!(r"[^EO]OOE", RE);
+    captures_len(input, &*RE)
+}
+
+fn o_1_empty_2_in_row(input: &str) -> i32 {
+    o_1_empty_2_in_row_right(input) + o_1_empty_2_in_row_left(input)
+}
+
+fn x_4_in_row(input: &str) -> i32 {
+    lazy_regex!(r"XXXX", RE);
+    captures_len(input, &*RE)
+}
+
+// this will count ExxxExxxE as one since the middle E gets consumed
+// method below will count the second sequence
+fn x_2_empty_3_in_row_single(input: &str) -> i32 {
+    lazy_regex!(r"EXXXE", RE);
+    captures_len(input, &*RE)
+}
+
+// this is funky because of a special case ExxxExxxE
+// so we will count the second ExxxE here
+fn x_2_empty_3_in_row_double(input: &str) -> i32 {
+    lazy_regex!(r"EXXXEXXXE", RE);
+    captures_len(input, &*RE)
+}
+
+fn x_2_empty_3_in_row(input: &str) -> i32 {
+    x_2_empty_3_in_row_single(input) + x_2_empty_3_in_row_double(input)
+}
+
+fn x_1_empty_3_in_row_left(input: &str) -> i32 {
+    lazy_regex!(r"EXXX[^EX]", RE);
+    captures_len(input, &*RE)
+}
+
+fn x_1_empty_3_in_row_right(input: &str) -> i32 {
+    lazy_regex!(r"[^EX]XXXE", RE);
+    captures_len(input, &*RE)
+}
+
+fn x_1_empty_3_in_row(input: &str) -> i32 {
+    x_1_empty_3_in_row_right(input) + x_1_empty_3_in_row_left(input)
+}
+
+fn x_2_empty_2_in_row_single(input: &str) -> i32 {
+    lazy_regex!(r"EXXE", RE);
+    captures_len(input, &*RE)
+}
+
+// this is funky because of a special case ExxxExxxE
+// so we will count the second ExxxE here
+fn x_2_empty_2_in_row_double(input: &str) -> i32 {
+    lazy_regex!(r"EXXEXXE", RE);
+    captures_len(input, &*RE)
+}
+
+fn x_2_empty_2_in_row(input: &str) -> i32 {
+    x_2_empty_2_in_row_single(input) + x_2_empty_2_in_row_double(input)
+}
+
+fn x_1_empty_2_in_row_left(input: &str) -> i32 {
+    lazy_regex!(r"EXX[^EX]", RE);
+    captures_len(input, &*RE)
+}
+
+fn x_1_empty_2_in_row_right(input: &str) -> i32 {
+    lazy_regex!(r"[^EX]XXE", RE);
+    captures_len(input, &*RE)
+}
+
+fn x_1_empty_2_in_row(input: &str) -> i32 {
+    x_1_empty_2_in_row_right(input) + x_1_empty_2_in_row_left(input)
+}
+
+pub fn heuristic_counter_x(input: &str) -> i32 {
+    let mut count: i32 = 0;
+    if (x_4_in_row(input) != 0) {
+        return 9999
+    }
+    if (o_4_in_row(input) != 0) {
+        return -9999
+    }
+
+    count += 5 * x_2_empty_3_in_row(input); //[# of two-side-open-3-in-a-row for me]
+    count -= 10 * o_2_empty_3_in_row(input); //[# of two-side-open-3-in-a-row for opponent]
+    count += 3 * x_1_empty_3_in_row(input); //[# of one-side-open-3-in-a-row for me]
+    count -= 6 * o_1_empty_3_in_row(input); //[# of one-side-open-3-in-a-row for opponent]
+    count += (x_2_empty_2_in_row(input) + x_1_empty_2_in_row(input)); //[# of open-2-in-a-row for me]
+    count -= (o_2_empty_2_in_row(input) + o_1_empty_2_in_row(input)); //[# of open-2-in-a-row for opponent]
+    count
+}
+
+pub fn heuristic_counter_o(input: &str) -> i32 {
+    let mut count: i32 = 0;
+    if (o_4_in_row(input) != 0) {
+        return 9999
+    }
+    if (x_4_in_row(input) != 0) {
+        return -9999
+    }
+
+    count += 5 * o_2_empty_3_in_row(input); //[# of two-side-open-3-in-a-row for me]
+    count -= 10 * x_2_empty_3_in_row(input); //[# of two-side-open-3-in-a-row for opponent]
+    count += 3 * o_1_empty_3_in_row(input); //[# of one-side-open-3-in-a-row for me]
+    count -= 6 * x_1_empty_3_in_row(input); //[# of one-side-open-3-in-a-row for opponent]
+    count += (o_2_empty_2_in_row(input) + o_1_empty_2_in_row(input)); //[# of open-2-in-a-row for me]
+    count -= (x_2_empty_2_in_row(input) + x_1_empty_2_in_row(input)); //[# of open-2-in-a-row for opponent]
+
+    count
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_x_2_empty_3_in_row() {
+        assert_eq!(x_2_empty_3_in_row_double("_EXXXE_"), 0);
+        assert_eq!(x_2_empty_3_in_row_double("_EXXXEXXX_"), 0);
+        assert_eq!(x_2_empty_3_in_row_double("_EXXXEEXXXE_"), 0);
+        assert_eq!(x_2_empty_3_in_row_double("_EXXXEXXXE_"), 1);
+
+        assert_eq!(x_2_empty_3_in_row_single("_EXXXE_"), 1);
+        assert_eq!(x_2_empty_3_in_row_single("_EEXXXEE_"), 1);
+        assert_eq!(x_2_empty_3_in_row_single("_EXXE_"), 0);
+        assert_eq!(x_2_empty_3_in_row_single("_EXXXEXXXE_"), 1);
+        assert_eq!(x_2_empty_3_in_row_single("_EXXXEEXXXE_"), 2);
+
+        assert_eq!(x_2_empty_3_in_row("_EXXXEXXXE_"), 2);
+        assert_eq!(x_2_empty_3_in_row("_EXXXEEXXXE_"), 2);
+    }
+
+    #[test]
+    fn test_x_1_empty_3_in_row() {
+        assert_eq!(x_1_empty_3_in_row("_EXXX_"), 1);
+        assert_eq!(x_1_empty_3_in_row("_EXXXEE_"), 0);
+        assert_eq!(x_1_empty_3_in_row("_EXXXO_"), 1);
+        assert_eq!(x_1_empty_3_in_row("_EXXXE_"), 0);
+        assert_eq!(x_1_empty_3_in_row("_XXXE_"), 1);
+
+        assert_eq!(x_1_empty_3_in_row("_EXXXEXXX_"), 1);
+        assert_eq!(x_1_empty_3_in_row("_EXXXOXXXE_"), 2);
+        assert_eq!(x_1_empty_3_in_row("_OXXXE_"), 1);
+        assert_eq!(x_1_empty_3_in_row("_EXXE_"), 0);
+        assert_eq!(x_1_empty_3_in_row("_XXXEXXXEXX_"), 1);
+    }
+
+
+    #[test]
+    fn test_x_2_empty_2_in_row() {
+        assert_eq!(x_2_empty_2_in_row("_EXXE_"), 1);
+        assert_eq!(x_2_empty_2_in_row("_EEXXEE_"), 1);
+        assert_eq!(x_2_empty_2_in_row("_EXXXE_"), 0);
+        assert_eq!(x_2_empty_2_in_row("_EXXXEXXE_"), 1);
+        assert_eq!(x_2_empty_2_in_row("_EXXEXXE_"), 2);
+        assert_eq!(x_2_empty_2_in_row("_EXXEEXXE_"), 2);
+    }
+
+    #[test]
+    fn test_x_1_empty_2_in_row() {
+        assert_eq!(x_1_empty_2_in_row("_EXX_"), 1);
+        assert_eq!(x_1_empty_2_in_row("_EXXEE_"), 0);
+        assert_eq!(x_1_empty_2_in_row("_EXXO_"), 1);
+        assert_eq!(x_1_empty_2_in_row("_EXXXE_"), 0);
+        assert_eq!(x_1_empty_2_in_row("_EXXX_"), 0);
+
+        assert_eq!(x_1_empty_2_in_row("_XXE_"), 1);
+        assert_eq!(x_1_empty_2_in_row("_EEXXE_"), 0);
+        assert_eq!(x_1_empty_2_in_row("_OXXE_"), 1);
+        assert_eq!(x_1_empty_2_in_row("_EXXE_"), 0);
+        assert_eq!(x_1_empty_2_in_row("_EEXXEE_"), 0);
+        assert_eq!(x_1_empty_2_in_row("_EXXEXX_"), 1);
+        assert_eq!(x_1_empty_2_in_row("_XXEEXX_"), 2);
+    }
+
+    #[test]
+    fn test_heuristic() {
+        let input = "_EEXXEE_";
+        assert_eq!(heuristic_counter_x(input), 1);
+        assert_eq!(x_2_empty_2_in_row(input), 1);
+        assert_eq!(x_1_empty_2_in_row(input), 0);
+
+        let mut count = 0;
+        count += 5 * x_2_empty_3_in_row(input); //[# of two-side-open-3-in-a-row for me]
+        assert_eq!(count, 0);
+        count -= 10 * o_2_empty_3_in_row(input); //[# of two-side-open-3-in-a-row for opponent]
+        assert_eq!(count, 0);
+        count += 3 * x_1_empty_3_in_row(input); //[# of one-side-open-3-in-a-row for me]
+        assert_eq!(count, 0);
+        count -= 6 * o_1_empty_3_in_row(input); //[# of one-side-open-3-in-a-row for opponent]
+        assert_eq!(count, 0);
+        count += (x_2_empty_2_in_row(input) + x_1_empty_2_in_row(input)); //[# of open-2-in-a-row for me]
+        assert_eq!(count, 1);
+        count -= (o_2_empty_2_in_row(input) + o_1_empty_2_in_row(input)); //[# of open-2-in-a-row for opponent]
+        assert_eq!(count, 1);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,342 @@
+#[macro_use] extern crate lazy_static;
+use std::cmp;
+use std::fmt;
+use rand::Rng;
+
+mod heuristic_counter;
+use heuristic_counter::{heuristic_counter_o, heuristic_counter_x};
+
+#[derive(Clone,Copy,Debug,PartialEq)]
+pub enum Symbol {
+    X,
+    O,
+    E
+}
+
+type Board = Vec<Symbol>;
+
+pub struct TicTacToe {
+    length: u32,
+    board: Board,
+    depth: u32,
+    player: Symbol,
+    opponent: Symbol,
+    turn: Symbol,
+    winner: Symbol,
+}
+
+fn xy_to_linear(x: u32, y: u32, h: u32, w: u32) -> Option<usize> {
+    if x > w - 1 || y > h - 1 {
+        None
+    } else {
+        let i = (y * w) + x;
+        Some(i as usize)
+    }
+}
+
+fn opposite_symbol(sym: &Symbol) -> Symbol {
+    match *sym {
+        Symbol::X => Symbol::O,
+        Symbol::O => Symbol::X,
+        Symbol::E => panic!("You can't pick empty!")
+    }
+}
+
+fn is_empty(sym: &Symbol) -> bool {
+    match sym {
+        Symbol::E => true,
+        _ => false,
+    }
+}
+
+fn player_heuristic(agent: &Symbol, input: &str) -> i32 {
+    match agent {
+        Symbol::X => heuristic_counter_x(input),
+        Symbol::O => heuristic_counter_o(input),
+        _ => panic!("Hey you cant use the empty char")
+    }
+}
+
+fn heuristic(board: &Board, l: u32, agent: &Symbol) -> i32 {
+    let mut heuristic_total = 0;
+
+    for i in 0..l {
+        let mut vertical_word = "_".to_string();
+        let mut horizontal_word = "_".to_string();
+        for j in 0..l {
+            let vertical_index = xy_to_linear(i, j, l, l).unwrap();
+            let horizontal_index = xy_to_linear(j, i, l, l).unwrap();
+            // println!("{} {}", vertical_index, horizontal_index);
+
+            vertical_word = format!("{}{:?}", vertical_word.clone(), board[vertical_index]);
+            horizontal_word = format!("{}{:?}", horizontal_word.clone(), board[horizontal_index]);
+        }
+        vertical_word = format!("{}_", vertical_word.clone());
+        horizontal_word = format!("{}_", horizontal_word.clone());
+        let v_heuristic = player_heuristic(agent, &vertical_word);
+        let h_heuristic = player_heuristic(agent, &horizontal_word);
+        heuristic_total += (v_heuristic + h_heuristic);
+        // println!("{} {}\n{} {}", vertical_word, v_heuristic, horizontal_word, h_heuristic);
+    }
+    // println!("---");
+
+    for k in 0..(l * 2) {
+        let mut vertical_word = "_".to_string();
+        let mut horizontal_word = "_".to_string();
+        for j in 0..k+1 {
+            let i = k - j;
+            if i < l && j < l {
+                let vertical_d_index = xy_to_linear(i, j, l, l).unwrap();
+                let horizontal_d_index = xy_to_linear(l - i - 1, j, l, l).unwrap();
+                vertical_word = format!("{}{:?}", vertical_word.clone(), board[vertical_d_index]);
+                horizontal_word = format!("{}{:?}", horizontal_word.clone(), board[horizontal_d_index]);
+                // println!("{} {}", vertical_d_index, horizontal_d_index);
+            }
+        }
+        vertical_word = format!("{}_", vertical_word.clone());
+        horizontal_word = format!("{}_", horizontal_word.clone());
+        let v_heuristic = player_heuristic(agent, &vertical_word);
+        let h_heuristic = player_heuristic(agent, &horizontal_word);
+        heuristic_total += (v_heuristic + h_heuristic);
+        // println!("{} {}\n{} {}", vertical_word, v_heuristic, horizontal_word, h_heuristic);
+    }
+
+    heuristic_total
+}
+
+fn terminal(board: &Board, length: u32) -> bool {
+    let h = heuristic(board, length, &Symbol::X);
+    if h > 99999 || h < -99999 {
+        true
+    } else {
+        false
+    }
+}
+
+fn minimax(node: &Board, length: u32, depth: u32, maximizing: bool, agent: &Symbol) -> i32 {
+    if depth == 0 || terminal(node, length) {
+        return heuristic(node, length, agent);
+    } else {
+        if maximizing {
+            let max_value = get_all_potential_moves(node, agent).iter().fold((-99999, vec![]), |acc, child| {
+                let value = minimax(child, length, depth - 1, false, &opposite_symbol(agent));
+                if value > acc.0 {
+                    (value, child.to_vec())
+                } else {
+                    acc
+                }
+            });
+            // println!("max {} {:?}", max_value.0, max_value.1);
+            return max_value.0;
+        } else {
+            let min_value = get_all_potential_moves(node, agent).iter().fold((99999, vec![]), |acc, child| {
+                let value = minimax(child, length, depth - 1, true, &opposite_symbol(agent));
+                if value < acc.0 {
+                    (value, child.to_vec())
+                } else {
+                    acc
+                }
+            });
+            // println!("min {} {:?}", min_value.0, min_value.1);
+            return min_value.0;
+        }
+    }
+}
+
+fn get_all_potential_moves(board: &Board, marker: &Symbol) -> Vec<Board> {
+    board.iter()
+        .enumerate()
+        .filter(|s| is_empty(s.1))
+        .map(|tupl| {
+            let mut newvec = board.clone();
+            newvec[tupl.0] = *marker;
+            newvec
+        })
+        .collect()
+}
+
+fn gen_random(length: u32) -> (u32, u32) {
+    // lazy_static! {
+    let mut rng = rand::thread_rng();
+    // }
+
+    let n: u32 = rng.gen();
+    let x = 1 + (n % (length - 1));
+    let h: u32 = rng.gen();
+    let y = 1 + (h % (length - 1));
+
+    (x, y)
+}
+
+impl TicTacToe {
+    pub fn new(player: Symbol, length: u32, depth: u32) -> Self {
+        let l = (length * length) as usize;
+        let board = vec![Symbol::E; l];
+        let opponent = opposite_symbol(&player);
+
+        TicTacToe {
+            length,
+            board,
+            depth,
+            player,
+            opponent,
+            turn: Symbol::X,
+            winner: Symbol::E,
+        }
+    }
+
+    pub fn make_move(&mut self, x: u32, y: u32) -> () {
+        let index = xy_to_linear(x, y, self.length, self.length);
+
+        if let Some(i) = index {
+            self.board[i as usize] = self.turn;
+            self.turn = opposite_symbol(&self.turn);
+        } else {
+            panic!("This is not a valid move!");
+        }
+    }
+
+    pub fn agent_move(&mut self) -> () {
+        let max_value = get_all_potential_moves(&self.board, &self.turn).iter().fold((-99999, vec![]), |acc, child| {
+            let value = minimax(child, self.length, self.depth, false, &self.turn);
+            if value > acc.0 {
+                (value, child.to_vec())
+            } else {
+                acc
+            }
+        });
+        println!("max {}", max_value.0);
+
+        if max_value.0 == 0 {
+            let (x, y) = gen_random(self.length);
+            while {
+                !is_empty(&self.board[xy_to_linear(x, y, self.length, self.length).unwrap()])
+            } {
+                let (x, y) = gen_random(self.length);
+            }
+
+            self.make_move(x, y);
+        } else {
+            self.board = max_value.1;
+            self.turn = opposite_symbol(&self.turn);
+        }
+    }
+
+    fn get_board(&self) -> &Board {
+        &self.board
+    }
+}
+
+// To use the `{}` marker, the trait `fmt::Display` must be implemented
+// manually for the type.
+impl fmt::Display for TicTacToe {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut string = "".to_string();
+
+        for tupl in self.board.iter().enumerate() {
+            let i = tupl.0;
+            let square = tupl.1;
+            let newline = "\n";
+            if i % (self.length as usize) == 0 {
+                string = format!("{}{}", string, newline);
+            }
+            let symbol = match square {
+                Symbol::X => "❌",
+                Symbol::O => "⭕️",
+                Symbol::E => "⬜️",
+            };
+
+            string = format!("{}{}", string, symbol);
+        }
+        write!(f, "{}", &string)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_xy_to_linear() {
+        let w = 5;
+        let h = 4;
+        assert_eq!(xy_to_linear( 1, 1, h, w), Some(6));
+        assert_eq!(xy_to_linear( 0, 0, h, w), Some(0));
+        assert_eq!(xy_to_linear( 4, 0, h, w), Some(4));
+        assert_eq!(xy_to_linear( 4, 2, h, w), Some(14));
+        assert_eq!(xy_to_linear( 4, 4, h, w), None);
+        assert_eq!(xy_to_linear( 5, 3, h, w), None);
+        assert_eq!(xy_to_linear( 1, 2, h, w), Some(11));
+    }
+
+    #[test]
+    fn test_move() {
+        let l = 3;
+        let mut ttt = TicTacToe::new(Symbol::X, l, 2);
+
+        ttt.make_move(0, 0);
+        ttt.make_move(1, 0);
+        let board = ttt.get_board();
+
+        assert_eq!(board[0], Symbol::X);
+        assert_eq!(board[1], Symbol::O);
+    }
+
+    #[test]
+    fn test_get_all_potential_moves() {
+        let l = 3;
+        let mut ttt = TicTacToe::new(Symbol::X, l, 2);
+
+        ttt.make_move(0, 0);
+        ttt.make_move(1, 0);
+
+        let moves = get_all_potential_moves(ttt.get_board(), &Symbol::X);
+        assert_eq!(moves.len(), 7);
+        // println!("{:?}", moves);
+        for (i, mov) in moves.iter().enumerate() {
+            assert_eq!(mov[i+2], Symbol::X);
+        }
+    }
+
+    #[test]
+    fn test_heuristic() {
+        let l = 6;
+        let mut ttt = TicTacToe::new(Symbol::X, l, 2);
+
+        ttt.make_move(2, 2); //x
+        ttt.make_move(2, 3);
+        ttt.make_move(3, 3); // x
+        ttt.make_move(1, 2);
+        ttt.make_move(1, 4); // x
+        ttt.make_move(1, 3);
+        ttt.make_move(2, 4); // x
+        ttt.make_move(0, 4);
+        ttt.make_move(1, 5); // x
+        ttt.make_move(3, 4);
+
+        assert_eq!(heuristic(ttt.get_board(), l, &Symbol::X), -8);
+    }
+
+    #[test]
+    fn test_agent_move() {
+        let l = 6;
+        let mut ttt = TicTacToe::new(Symbol::X, l, 2);
+
+        ttt.make_move(2, 2); //x
+        ttt.make_move(2, 3);
+        ttt.make_move(3, 3); // x
+        ttt.make_move(1, 2);
+        ttt.make_move(1, 4); // x
+        ttt.make_move(1, 3);
+        ttt.make_move(2, 4); // x
+        ttt.make_move(0, 4);
+        ttt.make_move(1, 5); // x
+        ttt.make_move(3, 4);
+
+        // println!("{:?}", ttt.get_board());
+        ttt.agent_move();
+        ttt.agent_move();
+        println!("{:?}", ttt.get_board());
+    }
+}
